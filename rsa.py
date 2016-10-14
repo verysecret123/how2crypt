56f3de94497900f1d0eed902f29b40d582576612
antoniob <antoniob@cs.ucsb.edu>
2016-10-14 04:37:11 -0700

    rsa.py TODOs

---
+++ rsa.py
@@ -0,0 +1,8 @@
+
+# TODO rsa stuff
+# show how to manually do rsa with modular pow and phi
+# explain encypt/decrypt/sign
+# message to number and number to message
+# padded/unpadded RSA
+# show how to import/export stuff from/to Crypto.PublicKey RSA
+# show CRT and how CRT is saved in ssh private keys
